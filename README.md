e2c0356c117f7ecf642db74a2fac19eb010be96a
Quentin Quadrat <lecrapouille@gmail.com>
2021-11-10 23:05:52 +0100

    Update script and adapt readme in accordance
    - Download explicit versions thirdparts (Godot and Brave)
    - Use or not Docker
    - If no thirdpart version is given do not update the thirdpart.
    - Adapt README.

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
 # Chreage setup and code source compilation
 
-*note* these steps are not yet fully complete ie calling Godot from Docker is not working.
-
 Chreage's development is depending on the following projects as third parts:
 - Brave-core: https://github.com/brave/brave-browser
 - Godot: https://github.com/godotengine/godot
 
-This document will explain how to setup and compile Chreage project using
-Docker. A docker can be useful, because, depending on the environment of your
-operating system, you may have more or less difficulties to compile third parts
-if you are trying to compile them
-[directly](https://github.com/chreage-rebirth/doc)) that is why we are offering
-a Dockerfile to allow you to have one correct compilation environment. You will
-just need two install two packages (`apt-get install` for example):
-- Docker (or podman which is a Docker without daemon and you can `alias
+This document will explain how to setup, download third-parts and compile
+Chreage project on Linux using the script `bootstrap.sh`. Depending on the
+environment of your operating system, you may have more or less difficulties to
+compile third parts if you are trying to compile them
+[directly](https://github.com/chreage-rebirth/doc)) that is why we are also
+offering a Dockerfile to allow you to have a correct compilation environment
+(this is still experimental since exporting the display an Vulkan for Docker can
+be difficult). You will just need two install two packages (`apt-get install`
+for example):
+- Docker (or Podman which is a Docker without daemon and you can `alias
   docker=podman` for following this document).
 - Git to download the code source and apply patches.
 
-Steps:
+## Steps
 
-- Export and ideally save this line inside your `~/.bashrc` file (or any equivalent file).
-Do not forget to replace `path/for/chreage_workspace` by the desired folder:
+- Export and ideally save this line inside your `~/.bashrc` file (or any
+equivalent file). Do not forget to replace `path/for/chreage_workspace` by the
+desired folder holding the whole project (let say around 100 gigabytes):
 ```bash
 export WORKSPACE_CHREAGE=path/for/chreage_workspace
 ```
@@ -31,22 +32,30 @@ git clone https://github.com/chreage-rebirth/bootstrap.git --depth=1
 cd bootstrap
 ```
 
+- You can edit some settings inside the script:
+  - The versions for Godot (currently `GODOT_VERSION=3.4-stable`) and Brave (currently `BRAVE_VERSION=v1.32.96`).
+  - If you want compiling using Docker (`USING_DOCKER=1`) or not (`USING_DOCKER=`).
+
 - Execute the bash script:
 ```bash
 ./bootstrap.sh $WORKSPACE_CHREAGE
 ```
 
-- After long hours of compilation, you can run Godot:
+- After long hours of compilation.
+  - If Docker was not used:
+    - you can run Godot: `$WORKSPACE_CHREAGE/godot/bin/godot.linuxbsd.tools.64`
+    - you can run Brave: `cd $WORKSPACE_CHREAGE; npm start Component`
+  - If Docker was used:
 ```bash
+TODO
+#Xephyr -resizeable :1 &
+#
+#docker run --rm -ti  --shm-size 1GB  -e DISPLAY=:1 -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/workspace -w /#workspace/brave-browser chreage:latest /bin/bash -c "/workspace/brave-browser/src/out/Component/brave --enable-#logging --v=0 --disable-brave-update --no-sandbox"
+#
 # $WORKSPACE_CHREAGE/godot/bin/godot.linuxbsd.tools.64
 ```
 
-- you can run Brave:
-```bash
-TODO
-```
-
-- If you want to modify the Chreage's code source and recompile it:
+- With Docker, if you want to modify the Chreage's code source and recompile it:
 ```bash
 cd $WORKSPACE_CHREAGE
 docker run --rm -ti -v $(pwd):/workspace -w /workspace chreage:latest /bin/bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,54 +1,145 @@
 #!/bin/bash -e
-### bash script allowing to download and compile Brave browser.
+###
+### bash script allowing to download and compile Brave browser and Godot.
+###
 
-# We are supposed to be inside the folder cloned by
-# https://github.com/chreage-rebirth/bootstrap
-# Save this path.
+### User settings. Desired versions. If you do not desire to install a
+### package unset the value
+GODOT_VERSION=3.4-stable
+BRAVE_VERSION=v1.32.96
+
+### User settings. Using Docker ? If yes set it with any value (WARNING:
+### CURRENTLY EXPERIMENTAL !!)
+USING_DOCKER=
+
+### We are supposed to be inside the root folder cloned by
+### https://github.com/chreage-rebirth/bootstrap. We save this path.
 HERE=`pwd`
 
-# Create the docker image if it does not exist. The docker image will offer all
-# packages needed to compile Chreage and its third parts such as Brave,
-# Godot ...
-docker build -t chreage .
+# Green color message
+function msg
+{
+    echo -e "\033[32m*** $*\033[00m"
+}
 
-# $1 is the path of the desired Chreage root folder to be created and holding
-# the whole code source of the project.
-WORKSPACE_CHREAGE="$1"
-if [ -z $WORKSPACE_CHREAGE ]; then
-   echo "Please define \$1 as the path for your workspace holding Chreage project (ie $0 ~/workspace_chreage)"
-   exit 1
+# Orange color message
+function info
+{
+    echo -e "\033[36m*** $*\033[00m"
+}
+
+# Red color message
+function err
+{
+    echo -e "\033[31m*** $*\033[00m"
+}
+
+### Check if docker is installed
+if [ -z "$USING_DOCKER" ]; then
+    info "You have not requested using docker"
+else
+    info "You have requested using docker"
+    USING_DOCKER=`echo which docker`
+    if [ -z "$USING_DOCKER" ]; then
+        err "It seems that you have no docker installed. Abort!"
+        exit 1
+    fi
 fi
+
+### Helper function calling docker with good params
+function docker_run()
+{
+    # Get the folder name (without the full path)
+    FOLDER="$1"
+    shift
+
+    if [ -z "$USING_DOCKER" ]; then
+        (cd $FOLDER && $*)
+    else
+        docker run --rm -ti -v $(pwd):/workspace -w /workspace/$WHERE \
+               chreage:latest /bin/bash -c "$*"
+    fi
+}
+
+### Create the docker image if it does not exist. The docker image will offer
+### all packages needed to compile Chreage and its third parts such as Brave,
+### Godot ...
+if [ ! -z "$USING_DOCKER" ]; then
+    docker build \
+           --build-arg USER=$USER --build-arg UID=$UID --build-arg GID=$GID \
+           -t chreage .
+fi
+
+### $1 is the path of the desired Chreage root folder to be created. It will
+### hold the whole code source of third-part projects.
+WORKSPACE_CHREAGE="$1"
+if [ -z "$WORKSPACE_CHREAGE" ]; then
+    err "Please define \$1 as the path for your workspace holding "
+    err "Chreage project (ie $0 ~/workspace_chreage). Abort!"
+    exit 1
+fi
+
+### Create and jump to Chreage root folder (or die if it does not exist).
+msg "Going to workspace $WORKSPACE_CHREAGE ..."
 mkdir -p $WORKSPACE_CHREAGE
 
-# Jump to Chreage root folder (or die if cannot).
-cd $WORKSPACE_CHREAGE || (echo "Cannot go to Chreage workspace"; exit 1)
+### Git clone or update Godot to the desired version.
+cd "$WORKSPACE_CHREAGE" || (err "Cannot go to Chreage workspace"; exit 1)
+if [ ! -z "$GODOT_VERSION" ]; then
+    GODOT_FOLDER="godot"
+    if [ -e $GODOT_FOLDER ]; then
+        msg "Updating Godot $GODOT_VERSION ..."
+        (cd $GODOT_FOLDER && git pull --depth=1)
+    else
+        msg "Cloning Godot $GODOT_VERSION ..."
+        git clone https://github.com/godotengine/godot.git --depth=1 -b $GODOT_VERSION
+    fi
 
-### Remove godot folder if already existing to allow git cloning.
-rm -fr godot
-git clone https://github.com/godotengine/godot.git --depth=1
+    # Call the docker against Chreage workspace to compile Godot ...
+    msg "Compiling Godot $GODOT_VERSION ..."
+    docker_run "$GODOT_FOLDER" "scons -j$(nproc) platform=linuxbsd"
+else
+    info "Ignoring Godot (explicitely set by the user)"
+fi
 
-### Remove brave-browser folder if already existing to allow git cloning.
-rm -fr brave-browser
+### Git clone or update brave-core's bootstraper to the desired version.
+cd "$WORKSPACE_CHREAGE" || (err "Cannot go to Chreage workspace"; exit 1)
+if [ ! -z "$BRAVE_VERSION" ]; then
+    BRAVE_FOLDER="brave-browser"
+    if [ -e $BRAVE_FOLDER ]; then
+        msg "Updating brave-browser $BRAVE_VERSION ..."
+        (cd $BRAVE_FOLDER &&  git pull --depth=1)
+        (cd $BRAVE_FOLDER/src && git pull --depth=1)
+    else
+        msg "Cloning brave-browser $BRAVE_VERSION ..."
+        git clone https://github.com/brave/brave-browser.git --depth=1 -b $BRAVE_VERSION
 
-# Download brave-browser (the bootstraper for compiling brave-core) and apply
-# patches to it and to brave-core such as forcing git cloning third-parts
-# without their whole history in the aim to save disk usage and save downloading
-# time.
-git clone https://github.com/brave/brave-browser.git --depth=1
-(cd brave-browser
- cp $HERE/patches/brave-browser/0001-Force-depth-1-to-reduce-size.patch .
-  git am --signoff < 0001-Force-depth-1-to-reduce-size.patch
-  cd src
-  git clone https://github.com/brave/brave-core.git --depth=1 brave
-  cd brave
-  cp $HERE/patches/brave-core/0001-Force-git-cloning-brave-core-with-depth-1-to-save-di.patch .
-  cp $HERE/patches/brave-core/0002-Do-not-care-to-be-root-when-inside-docker.patch .
-  git am --signoff < 0001-Force-git-cloning-brave-core-with-depth-1-to-save-di.patch
-  git am --signoff < 0002-Do-not-care-to-be-root-when-inside-docker.patch
-)
+        msg "Patching brave-browser $BRAVE_VERSION ..."
+        (cd $BRAVE_FOLDER
+         cp $HERE/patches/brave-browser/0001-Force-depth-1-to-reduce-size.patch .
+         git am --signoff < 0001-Force-depth-1-to-reduce-size.patch
+         cd src
+         git clone https://github.com/brave/brave-core.git --depth=1 -b $BRAVE_VERSION brave
+         cd brave
+         cp $HERE/patches/brave-core/0001-Force-git-cloning-brave-core-with-depth-1-to-save-di.patch .
+         cp $HERE/patches/brave-core/0002-Do-not-care-to-be-root-when-inside-docker.patch .
+         git am --signoff < 0001-Force-git-cloning-brave-core-with-depth-1-to-save-di.patch
+         git am --signoff < 0002-Do-not-care-to-be-root-when-inside-docker.patch
+        )
+    fi
 
-# Go inside your Chreage workspace and call the docker against your workspace to
-# compile Brave, Godot ...
-cd $WORKSPACE_CHREAGE
-docker run --rm -ti -v $(pwd):/workspace -w /workspace/godot chreage:latest /bin/bash -c "scons -j$(nproc) platform=linuxbsd"
-docker run --rm -ti -v $(pwd):/workspace -w /workspace/brave-browser chreage:latest /bin/bash -c "npm install; npm run init; npm run build"
+    # Call the docker against Chreage workspace to compile Brave ...
+    if [ -f $WORKSPACE_CHREAGE/$BRAVE_FOLDER/out/brave ]; then
+        msg "Synchronizing brave-browser $BRAVE_VERSION ..."
+        docker_run "$BRAVE_FOLDER" "npm run sync"
+    else
+        msg "Compiling brave-browser $BRAVE_VERSION ..."
+        docker_run "$BRAVE_FOLDER" "npm install"
+        docker_run "$BRAVE_FOLDER" "npm run init"
+    fi
+    docker_run "$BRAVE_FOLDER" "npm run build Component"
+else
+    info "Ignoring Brave (explicitely set by the user)"
+fi
+
+info "DONE"
